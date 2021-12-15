5faba9a14269385c7fe64177c7f38e62e55eac61
pardeshisanjay18 <pardeshisanjay18@gmail.com>
2021-12-15 22:47:51 +0530

    FirstCommit

---
+++ ApplicationLifeCycle/App.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace ApplicationLifeCycle
+{
+
+    //public partial class App : Application
+    //{
+    //    [STAThread()]
+    //    public static void Main()
+    //    {
+    //        TestApplication.App app = new TestApplication.App();
+    //        app.InitializeComponent();
+    //        app.Run();
+    //    }
+    //    public void InitializeComponent()
+    //    {
+    //        this.StartupUri = new Uri("Window1.xaml", System.UriKind.Relative);
+    //    }
+    //} 
+    public partial class App : Application
+    {
+
+        public App() { }
+        private bool unsavedData = false;
+        public bool UnsavedData
+        {
+            get { return unsavedData; }
+            set { unsavedData = value; }
+        }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            UnsavedData = true;
+        }
+
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("An unhandled " + e.Exception.GetType().ToString() +
+" exception was caught and ignored.");
+            e.Handled = true;
+
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+
+        }
+
+        private void Application_Activated(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Application_Deactivated(object sender, EventArgs e)
+        {
+
+        }      
+
+        private void Application_SessionEnding(object sender, SessionEndingCancelEventArgs e)
+        {
+            base.OnSessionEnding(e);
+            if (UnsavedData)
+            {
+                e.Cancel = true;
+                MessageBox.Show(
+                "The application attempted to be closed as a result of " +
+                e.ReasonSessionEnding.ToString() +
+                ". This is not allowed, as you have unsaved data.");
+            }
+
+        }
+    }
+}
---
+++ ApplicationLifeCycle/Startup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace ApplicationLifeCycle
+{
+    public class Startup
+    {       
+        [STAThread]
+        static void Main()
+        { 
+
+            //===========First Approach===============
+            //Create application
+            Application app = new Application();
+            //Create a main window
+            login win = new login();
+            //Lauch the application and show the window
+            app.Run(win);
+
+            //===========Second Approach=============
+
+            //// Create the application.
+            //Application app = new Application();
+            //// Create, assign, and show the main window.
+            //Window1 win = new Window1();
+            //app.MainWindow = win;
+            //win.Show();
+            //// Keep the application alive.
+            //app.Run();
+        }
+    }
+}
